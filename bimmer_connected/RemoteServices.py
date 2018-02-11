daf43e4eafbe9cd267253149d8475b1544d35c31
M1n3rva <m1n3rva@gmx.net>
2018-02-11 15:51:29 +0100

    parsing datetime works

---
+++ bimmer_connected/RemoteServices.py
@@ -0,0 +1,26 @@
+from enum import Enum
+import datetime
+
+TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+
+
+class ExecutionState(Enum):
+    PENDING = 'PENDING'
+    DELIVERED = 'DELIVERED_TO_VEHICLE'
+    EXECUTED = 'EXECUTED'
+
+
+class RemoteServiceStatus(object):
+
+    def __init__(self, response: dict):
+        self._response = response
+        self.state = response['remoteServiceStatus']
+        self.timestamp = self._parse_timestamp(response)
+
+    @staticmethod
+    def _parse_timestamp(timestamp: str) -> datetime.datetime:
+        offset = int(timestamp[-3:])
+        tz = datetime.timezone(datetime.timedelta(hours=offset))
+        result = datetime.datetime.strptime(timestamp[:-3], TIME_FORMAT)
+        result.replace(tzinfo=tz)
+        return result
