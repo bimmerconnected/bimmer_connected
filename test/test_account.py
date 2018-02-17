1064f4a776a872ed670e921ef969af07fd67bbb0
M1n3rva <m1n3rva@gmx.net>
2018-02-17 14:52:47 +0100

    tests for creating account passing
    * added logging for server response
    * added backend mock

---
+++ test/test_account.py
@@ -0,0 +1,24 @@
+"""Tests for ConnectedDriveAccount."""
+import unittest
+from unittest import mock
+from bimmer_connected import ConnectedDriveAccount
+from test import BackendMock
+
+TEST_USERNAME = 'some_user'
+TEST_PASSWORD = 'my_secret'
+TEST_COUNTRY = 'Germany'
+
+
+class TestAccount(unittest.TestCase):
+    """Tests for ConnectedDriveAccount."""
+
+    def test_token_vehicles(self):
+        """Test getting backend token and vehicle list."""
+        backend_mock = BackendMock()
+        with mock.patch('bimmer_connected.requests', new=backend_mock):
+            account = ConnectedDriveAccount(TEST_USERNAME, TEST_PASSWORD, TEST_COUNTRY)
+            self.assertIsNotNone(account._oauth_token)
+            vehicles = account.vehicles
+            self.assertEqual(1, len(vehicles))
+            self.assertEqual(vehicles[0].vin, 'G31_NBTEvo_VIN')
+
