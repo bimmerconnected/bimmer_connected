c38d86c1f92fc79884a51d49e938eda0666da5ec
Christian Kühnel <christian.kuehnel@gmail.com>
2018-02-18 19:41:43 +0100

    added the readme.rst to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from setuptools import setup, find_packages
 
 from bimmer_connected.const import __version__ as version
 
+
+def readme():
+    """Load the readme file."""
+    with open('README.rst') as readme_file:
+        return readme_file.read()
+
+
 setup(
     name='bimmer_connected',
     version=version,
     description='Library to read data from the BMW Connected Drive portal',
+    long_description=readme(),
     url='https://github.com/ChristianKuehnel/bimmer_connected',
     author='Christian Kühnel',
     author_email='christian.kuehnel@gmail.com',
@@ -14,10 +22,10 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: System :: Hardware :: Hardware Drivers',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(),
     install_requires=['requests', 'typing>=3,<4'],
