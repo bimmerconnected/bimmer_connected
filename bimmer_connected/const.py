36db70209fddf8e1f7475d3803f606096b1e17d5
Christian Kühnel <christian.kuehnel@gmail.com>
2018-02-24 15:07:25 +0100

    fixed version number

--- bimmer_connected/const.py
+++ bimmer_connected/const.py
@@ -1,7 +1,7 @@
 """Version numbers of bimmer_connected."""
 MAJOR_VERSION = 0
 MINOR_VERSION = 4
-PATCH_VERSION = '0_dev'
+PATCH_VERSION = '0'
 
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
