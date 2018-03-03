fce86c0509417264f12aa2887ff73dbeb378c573
M1n3rva <m1n3rva@gmx.net>
2018-03-03 09:51:27 +0100

    added more tests

--- test/test_vehicle.py
+++ test/test_vehicle.py
@@ -1,8 +1,10 @@
 """Tests for ConnectedDriveVehicle."""
 import unittest
-from test import load_response_json
+from unittest import mock
+from test import load_response_json, BackendMock, TEST_USERNAME, TEST_PASSWORD, TEST_COUNTRY
 
 from bimmer_connected.vehicle import ConnectedDriveVehicle, DriveTrainType
+from bimmer_connected.account import ConnectedDriveAccount
 
 G31_VEHICLE = load_response_json('vehicles.json')[0]
 F48_VEHICLE = load_response_json('vehicles.json')[1]
@@ -29,3 +31,14 @@ class TestVehicle(unittest.TestCase):
 
         vehicle = ConnectedDriveVehicle(None, I01_VEHICLE)
         self.assertEqual(DriveTrainType.BEV, vehicle.drive_train)
+
+    def test_parsing_attributes(self):
+        """Test parsing different attributes of the vehicle."""
+        backend_mock = BackendMock()
+        with mock.patch('bimmer_connected.account.requests', new=backend_mock):
+            account = ConnectedDriveAccount(TEST_USERNAME, TEST_PASSWORD, TEST_COUNTRY)
+
+        for vehicle in account.vehicles:
+            self.assertIsNotNone(vehicle.drive_train)
+            self.assertIsNotNone(vehicle.name)
+            self.assertIsNotNone(vehicle.has_rex)
