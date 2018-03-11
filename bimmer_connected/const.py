100c34dc1f65495cacd8ac1cbf5b18970a5f3e90
M1n3rva <m1n3rva@gmx.net>
2018-03-11 12:19:17 +0100

    incremented version number

--- bimmer_connected/const.py
+++ bimmer_connected/const.py
@@ -1,7 +1,7 @@
 """Version numbers of bimmer_connected."""
 MAJOR_VERSION = 0
-MINOR_VERSION = 4
-PATCH_VERSION = '2'
+MINOR_VERSION = 5
+PATCH_VERSION = 0
 
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
