de2686b183d527ef0e662b7ceee86c4884ad77fa
m1n3rva <m1n3rva@gmx.net>
2018-05-02 21:36:29 +0200

    made all self._attributes accesses via []
    so that we get a KeyError when someone access a non existing attribute.
    Also fixed the tests.

--- bimmer_connected/state.py
+++ bimmer_connected/state.py
@@ -66,11 +66,11 @@ def backend_parameter(func):
     def _func_wrapper(self: 'VehicleState', *args, **kwargs):
         # pylint: disable=protected-access
         if self._attributes is None:
-            raise ValueError('No data available!')
+            raise ValueError('No data available for vehicle status!')
         try:
             return func(self, *args, **kwargs)
         except KeyError:
-            _LOGGER.error('No data available!')
+            _LOGGER.error('No data available for attribute %s!', str(func))
             return None
     return _func_wrapper
 
@@ -96,7 +96,7 @@ class VehicleState(object):  # pylint: disable=too-many-public-methods
 
     @property
     @backend_parameter
-    def attributes(self) -> datetime.datetime:
+    def attributes(self) -> dict:
         """Retrieve all attributes from the sever.
 
         This does not parse the results in any way.
@@ -118,11 +118,11 @@ class VehicleState(object):  # pylint: disable=too-many-public-methods
         This only provides data, if the vehicle tracking is enabled!
         """
         pos = self._attributes['position']
-        if self.is_vehicle_tracking_enabled:
-            return float(pos['lat']), float(pos['lon'])
-        else:
-            _LOGGER.warning('Positioning status of %s is %s', self._vehicle.vin, pos['status'])
-        return None
+        if not self.is_vehicle_tracking_enabled:
+            _LOGGER.warning('Positioning status is %s', pos['status'])
+            return None
+        pos = self._attributes['position']
+        return float(pos['lat']), float(pos['lon'])
 
     @property
     @backend_parameter
@@ -254,13 +254,13 @@ class VehicleState(object):  # pylint: disable=too-many-public-methods
 
     def __getattr__(self, item):
         """Generic get function for all backend attributes."""
-        return self._attributes.get(item)
+        return self._attributes[item]
 
     @property
     @backend_parameter
     def remaining_range_electric(self) -> int:
         """Remaining range on battery, in kilometers."""
-        return self._attributes.get('remainingRangeElectric')
+        return int(self._attributes['remainingRangeElectric'])
 
     @property
     @backend_parameter
@@ -280,31 +280,27 @@ class VehicleState(object):  # pylint: disable=too-many-public-methods
     @backend_parameter
     def max_range_electric(self) -> int:
         """ This can change with driving style and temperature in kilometers."""
-        return int(self._attributes.get('maxRangeElectric'))
+        return int(self._attributes['maxRangeElectric'])
 
     @property
     @backend_parameter
     def charging_status(self) -> ChargingState:
         """Charging state of the vehicle."""
-        state = self._attributes.get('chargingStatus')
-        if state is None:
-            return None
+        state = self._attributes['chargingStatus']
         return ChargingState(state)
 
     @property
     @backend_parameter
     def charging_time_remaining(self) -> datetime.timedelta:
         """Get the remaining charging time."""
-        minutes = self._attributes.get('chargingTimeRemaining')
-        if minutes is None:
-            return None
+        minutes = self._attributes['chargingTimeRemaining']
         return datetime.timedelta(minutes=minutes)
 
     @property
     @backend_parameter
     def charging_level_hv(self) -> int:
         """State of charge of the high voltage battery in percent."""
-        return int(self._attributes.get('chargingLevelHv'))
+        return int(self._attributes['chargingLevelHv'])
 
     @property
     @backend_parameter
@@ -314,7 +310,7 @@ class VehicleState(object):  # pylint: disable=too-many-public-methods
         Right now they are not parsed, as we do not have sample data with CC messages.
         See issue https://github.com/m1n3rva/bimmer_connected/issues/55
         """
-        return self._attributes.get('checkControlMessages')
+        return self._attributes['checkControlMessages']
 
     @property
     @backend_parameter
