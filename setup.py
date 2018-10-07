57dfcb7aea433094661233dd8f385609a9143163
m1n3rva <m1n3rva@gmx.net>
2018-10-07 12:02:42 +0200

    now using pbr to manage version numbers automagically

--- setup.py
+++ setup.py
@@ -1,36 +1,7 @@
 """Python package description."""
-from setuptools import setup, find_packages
-
-from bimmer_connected.const import __version__ as version
-
-
-def readme():
-    """Load the readme file."""
-    with open('README.rst') as readme_file:
-        return readme_file.read()
-
+from setuptools import setup
 
 setup(
-    name='bimmer_connected',
-    version=version,
-    description='Library to read data from the BMW Connected Drive portal',
-    long_description=readme(),
-    url='https://github.com/m1n3rva/bimmer_connected',
-    author='m1n3rva',
-    author_email='m1n3rva@gmx.net',
-    license='Apache 2.0',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    packages=find_packages(),
-    install_requires=['requests', 'typing>=3,<4'],
-    keywords='BMW Connected Drive home automation',
-    zip_safe=False,
-    extras_require={'testing': ['pytest']},
-    scripts=['bimmerconnected'],
+    setup_requires=['pbr'],
+    pbr=True,
 )
