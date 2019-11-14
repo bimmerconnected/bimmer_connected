cdaac788b4779acddb41540d5c5d1efbd016528f
robthebold <38596885+robthebold@users.noreply.github.com>
2019-11-14 16:22:48 -0600

    added parsers for new functions

--- bimmer_connected/cli.py
+++ bimmer_connected/cli.py
@@ -56,6 +56,36 @@ def main() -> None:
                                 nargs='?', default=None)
     sendpoi_parser.set_defaults(func=send_poi)
 
+    chargenow_parser = subparsers.add_parser('chargenow', description='start vehicle charing immediately')
+    _add_default_arguments(chargenow_parser)
+    chargenow_parser.add_argument('vin', help='vehicle identification number')
+    chargenow_parser.set_defaults(func=charge_now)
+
+    getlasttrip_parser = subparsers.add_parser('lasttrip', description='get last trip details')
+    _add_default_arguments(getlasttrip_parser)
+    getlasttrip_parser.add_argument('vin', help='vehicle identification number')
+    getlasttrip_parser.set_defaults(func=get_last_trip)
+    
+    getalltrips_parser = subparsers.add_parser('alltrips', description='get all trips efficiency data')
+    _add_default_arguments(getalltrips_parser)
+    getalltrips_parser.add_argument('vin', help='vehicle identification number')
+    getalltrips_parser.set_defaults(func=get_all_trips)
+
+    getchargingprofile_parser = subparsers.add_parser('chargingprofile', description='get charging schedules')
+    _add_default_arguments(getchargingprofile_parser)
+    getchargingprofile_parser.add_argument('vin', help='vehicle identification number')
+    getchargingprofile_parser.set_defaults(func=get_vehicle_charging_profile)
+    
+    getdestinations_parser = subparsers.add_parser('destinations', description='get all destinations')
+    _add_default_arguments(getdestinations_parser)
+    getdestinations_parser.add_argument('vin', help='vehicle identification number')
+    getdestinations_parser.set_defaults(func=get_vehicle_destinations)
+   
+    getrangemap_parser = subparsers.add_parser('rangemap', description='get range map')
+    _add_default_arguments(getrangemap_parser)
+    getrangemap_parser.add_argument('vin', help='vehicle identification number')
+    getrangemap_parser.set_defaults(func=get_vehicle_rangemap)
+
     args = parser.parse_args()
     args.func(args)
 
@@ -110,6 +140,18 @@ def light_flash(args) -> None:
     print(status.state)
 
 
+def charge_now(args) -> None:
+    """Trigger the vehicle to charge immediately."""
+    account = ConnectedDriveAccount(args.username, args.password, get_region_from_name(args.region))
+    vehicle = account.get_vehicle(args.vin)
+    if not vehicle:
+        valid_vins = ", ".join(v.vin for v in account.vehicles)
+        print('Error: Could not find vehicle for VIN "{}". Valid VINs are: {}'.format(args.vin, valid_vins))
+        return
+    status = vehicle.remote_services.trigger_remote_charge_now()
+    print(status.state)
+
+
 def image(args) -> None:
     """Download a rendered image of the vehicle."""
     account = ConnectedDriveAccount(args.username, args.password, get_region_from_name(args.region))
@@ -119,6 +161,56 @@ def image(args) -> None:
         image_data = vehicle.get_vehicle_image(400, 400, VehicleViewDirection.FRONT)
         output_file.write(image_data)
     print('vehicle image saved to image.png')
+
+def get_last_trip(args) -> json:
+    """Downlad details of last trip"""
+    account = ConnectedDriveAccount(args.username, args.password, get_region_from_name(args.region))
+    vehicle = account.get_vehicle(args.vin)
+    if not vehicle:
+        valid_vins = ", ".join(v.vin for v in account.vehicles)
+        print('Error: Could not find vehicle for VIN "{}". Valid VINs are: {}'.format(args.vin, valid_vins))
+        return
+    print(json.dumps(vehicle.get_vehicle_lasttrip(),indent=4))    
+
+def get_all_trips(args) -> json:
+    """Downlad statistics of all trips"""
+    account = ConnectedDriveAccount(args.username, args.password, get_region_from_name(args.region))
+    vehicle = account.get_vehicle(args.vin)
+    if not vehicle:
+        valid_vins = ", ".join(v.vin for v in account.vehicles)
+        print('Error: Could not find vehicle for VIN "{}". Valid VINs are: {}'.format(args.vin, valid_vins))
+        return
+    print(json.dumps(vehicle.get_vehicle_alltrips(),indent=4))
+
+def get_vehicle_charging_profile(args) -> json:
+    """Download one-time and weekly charging schedules and settings"""
+    account = ConnectedDriveAccount(args.username, args.password, get_region_from_name(args.region))
+    vehicle = account.get_vehicle(args.vin)
+    if not vehicle:
+        valid_vins = ", ".join(v.vin for v in account.vehicles)
+        print('Error: Could not find vehicle for VIN "{}". Valid VINs are: {}'.format(args.vin, valid_vins))
+        return
+    print(json.dumps(vehicle.get_vehicle_charging_profile(),indent=4))
+
+def get_vehicle_destinations(args) -> json:
+    """Shows the destinations you've previously sent to the car."""
+    account = ConnectedDriveAccount(args.username, args.password, get_region_from_name(args.region))
+    vehicle = account.get_vehicle(args.vin)
+    if not vehicle:
+        valid_vins = ", ".join(v.vin for v in account.vehicles)
+        print('Error: Could not find vehicle for VIN "{}". Valid VINs are: {}'.format(args.vin, valid_vins))
+        return
+    print(json.dumps(vehicle.get_vehicle_destinations(),indent=4))
+
+def get_vehicle_rangemap(args) -> json:
+    """Get a set of lat/lon points defining a polygon bounding vehicle range"""
+    account = ConnectedDriveAccount(args.username, args.password, get_region_from_name(args.region))
+    vehicle = account.get_vehicle(args.vin)
+    if not vehicle:
+        valid_vins = ", ".join(v.vin for v in account.vehicles)
+        print('Error: Could not find vehicle for VIN "{}". Valid VINs are: {}'.format(args.vin, valid_vins))
+        return
+    print(json.dumps(vehicle.get_vehicle_rangemap(),indent=4))
 
 
 def send_poi(args) -> None:
