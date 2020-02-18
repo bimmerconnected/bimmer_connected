e0504c7624c17b16e05dee4c7b3d5397e59891f1
Gerard <gerard33@users.noreply.github.com>
2020-02-18 23:39:37 +0100

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -36,8 +36,9 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
