af77f64dd579c3308ec0ccdfd4708b4459f73e48
EddyK69 <eduard@kuijt-evers.net>
2020-11-02 21:20:24 +0100

    Added vehicle position-heading

--- bimmer_connected/vehicle_status.py
+++ bimmer_connected/vehicle_status.py
@@ -145,6 +145,20 @@ class VehicleStatus:  # pylint: disable=too-many-public-methods
 
     @property
     @backend_parameter
+    def gps_heading(self) -> (int):
+        """Get the last known heading of the vehicle.
+
+        This only provides data, if the vehicle tracking is enabled!
+        """
+        pos = self._state.attributes[SERVICE_STATUS]['position']
+        if not self.is_vehicle_tracking_enabled:
+            _LOGGER.warning('Positioning status is %s', pos['status'])
+            return None
+        pos = self._state.attributes[SERVICE_STATUS]['position']
+        return int(pos['heading'])
+
+    @property
+    @backend_parameter
     def is_vehicle_tracking_enabled(self) -> bool:
         """Check if the position tracking of the vehicle is enabled.
 
