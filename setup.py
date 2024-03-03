77b4d3f8361f0cf64ffe046bf71e30eb4126dc91
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-03-03 23:26:23 +0100

    Move extras_require to setup.cfg (#601)
    * Move extras_require to setup.cfg
    
    * Move install_requires to setup.cfg

--- setup.py
+++ setup.py
@@ -4,14 +4,4 @@ from setuptools import setup
 setup(
     setup_requires=["pbr"],
     pbr=True,
-    install_requires=[
-        "httpx",
-        "pycryptodome>=3.4",
-        "pyjwt>=2.1.0",
-    ],
-    extras_require={
-        "china": [
-            "Pillow",
-        ],
-    },
 )
