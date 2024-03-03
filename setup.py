e832b8cc57422424c224acd45b4ae5bfb022152a
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-03-03 19:53:14 +0100

    Move install_requires to setup.cfg

--- setup.py
+++ setup.py
@@ -4,9 +4,4 @@ from setuptools import setup
 setup(
     setup_requires=["pbr"],
     pbr=True,
-    install_requires=[
-        "httpx",
-        "pycryptodome>=3.4",
-        "pyjwt>=2.1.0",
-    ],
 )
